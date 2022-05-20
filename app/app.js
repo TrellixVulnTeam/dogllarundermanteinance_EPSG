2ee9852c64b9acd93911dabb124c92103905730a
MatiMontenegro <103597788+MatiMontenegro@users.noreply.github.com>
2022-05-20 12:57:18 -0300

    first commit under manteinance

---
+++ app/app.js
@@ -0,0 +1,40 @@
+//Intro JavaScript Config
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(() =>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(() =>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+
+                }, (idx + 10) * 500)
+            })
+        }, 5000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh'
+        }, 5000)
+    })
+})
+//end
+
+//Button Menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }  //end
